6b7084b371044f5682b928856f37bbcdad36b431
Roberto Poveda <rapprj4500@gmail.com>
2023-12-10 20:11:11 -0500

    Agregando el type button y validando que los inputs sean números para que se puedan sumar

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const h1 = document.querySelector('h1');
 const form = document.querySelector('#form'); 
 const input1 = document.querySelector('#calculo1');
-const input1 = document.querySelector('#calculo2');
+const input2 = document.querySelector('#calculo2');
 const btn = document.querySelector('#btnCalcular');
 const pResult = document.querySelector('#result');
 
@@ -19,6 +19,6 @@ btn.addEventListener('click', sumarInputValues)
 function sumarInputValues(event) {
   // console.log({event});
   // event.preventDefault();
-  const sumaInputs = input1.value + input2.value;
+  const sumaInputs = parseFloat(input1.value) + parseFloat(input2.value);
   pResult.innerText = "Resultado: " + sumaInputs;
 }
